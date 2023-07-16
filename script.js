63d3e905e0e5c4e5f6a313cd7300e99d20cd5a3f
Shreyas <shreyas.mohanty03@gmail.com>
2023-07-16 17:46:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const toggleBtn = document.querySelector('.toggle-btn')
+const toggleBtnIcon = document.querySelector('.toggle-btn i')
+const dropDownMenu = document.querySelector('.dropdown-menu')
+
+toggleBtn.onclick = ()=> {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+    toggleBtnIcon.classList = isOpen?"fa-solid fa-xmark":"fa-solid fa-bars";
+}
+
+// ==============NAVBAR HIDE SCROLL=================
+
+let prevScrollpos = window.scrollY;
+window.onscroll = ()=>{
+    let currentScrollpos = window.scrollY;
+    if(prevScrollpos>currentScrollpos)
+    {
+        document.querySelector('.navbar').style.top = '0';
+    }
+    else{
+        document.querySelector('.navbar').style.top = '-60px';
+    }
+    prevScrollpos = currentScrollpos;
+}
+// ==================================================
+const changingName = document.querySelector('.change-service');
+const features = [
+    "Social Media Content",
+    "Podcasting",
+    "App Development Services",
+    "informative content",
+  ];
+  let i = 0;
+  
+  const featureName = (array, num) => {
+    return array[num];
+  };
+  
+  setInterval(() => {
+    changingName.classList.add("changingName-hidden");
+    setTimeout(() => {
+      changingName.classList.remove("changingName-hidden");
+      if (i === 4) {
+        i = 0;
+      }
+      const nextName = featureName(features, i);
+      changingName.textContent = nextName;
+      i++;
+    }, 800);
+  }, 1500);
+//   ===========================================================
