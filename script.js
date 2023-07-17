e4e2a7e61ee7d41f476bbdb9ed8d149f40ecf719
Shreyas <shreyas.mohanty03@gmail.com>
2023-07-17 14:42:57 +0530

    updated

--- script.js
+++ script.js
@@ -1,57 +1,56 @@
-const toggleBtn = document.querySelector('.toggle-btn')
-const toggleBtnIcon = document.querySelector('.toggle-btn i')
-const dropDownMenu = document.querySelector('.dropdown-menu')
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropDownMenu = document.querySelector(".dropdown-menu");
 
-toggleBtn.onclick = ()=> {
-    dropDownMenu.classList.toggle('open')
-    const isOpen = dropDownMenu.classList.contains('open')
-    toggleBtnIcon.classList = isOpen?"fa-solid fa-xmark":"fa-solid fa-bars";
-}
+toggleBtn.onclick = () => {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
 
 // ==============NAVBAR HIDE SCROLL=================
 
 let prevScrollpos = window.scrollY;
-window.onscroll = ()=>{
-    let currentScrollpos = window.scrollY;
-    if(prevScrollpos>currentScrollpos)
-    {
-        document.querySelector('.navbar').style.top = '0';
+window.onscroll = () => {
+  let currentScrollpos = window.scrollY;
+  if (prevScrollpos > currentScrollpos) {
+    document.querySelector(".navbar").style.top = "0";
+  } else {
+    const condition = dropDownMenu.classList.contains("open");
+    if (condition) {
+      dropDownMenu.classList.toggle("open");
+      toggleBtnIcon.classList = condition
+        ? "fa-solid fa-bars"
+        : "fa-solid fa-xmark";
     }
-    else{
-        const condition = dropDownMenu.classList.contains('open');
-        if(condition)
-        {
-          dropDownMenu.classList.toggle('open')
-          toggleBtnIcon.classList = condition?"fa-solid fa-bars":"fa-solid fa-xmark";
-        }
-        document.querySelector('.navbar').style.top = '-60px';
-    }
-    prevScrollpos = currentScrollpos;
-}
+    document.querySelector(".navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollpos;
+};
 // ==================================================
-const changingName = document.querySelector('.change-service');
+const changingName = document.querySelector(".change-service");
 const features = [
-    "Social Media Content",
-    "Podcasting",
-    "App Development Services",
-    "informative content",
-  ];
-  let i = 0;
-  
-  const featureName = (array, num) => {
-    return array[num];
-  };
-  
-  setInterval(() => {
-    changingName.classList.add("changingName-hidden");
-    setTimeout(() => {
-      changingName.classList.remove("changingName-hidden");
-      if (i === 4) {
-        i = 0;
-      }
-      const nextName = featureName(features, i);
-      changingName.textContent = nextName;
-      i++;
-    }, 800);
-  }, 1500);
+  "Social Media Content",
+  "Podcasting",
+  "App Development Services",
+  "informative content",
+];
+let i = 0;
+
+const featureName = (array, num) => {
+  return array[num];
+};
+
+setInterval(() => {
+  changingName.classList.add("changingName-hidden");
+  setTimeout(() => {
+    changingName.classList.remove("changingName-hidden");
+    if (i === 4) {
+      i = 0;
+    }
+    const nextName = featureName(features, i);
+    changingName.textContent = nextName;
+    i++;
+  }, 800);
+}, 1500);
 //   ===========================================================
